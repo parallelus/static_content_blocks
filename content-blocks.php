a1caeb442719a09f0e6aad0ec1f14248381e7d4d
Andy <andy@parallelus.net>
2016-03-29 14:10:24 -0500

    Updated with plugin files.

---
+++ content-blocks.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Static Content Blocks
+Plugin URI: http://para.llel.us
+Description: Content sections to easily add content to theme sections.
+Author: Parallelus
+Author URI: http://para.llel.us
+Version: 1.0.2
+*/
+
+if (!function_exists('the_static_block')) {
+	require_once( plugin_dir_path( __FILE__ ) . 'post-type-static-block.php' );
+}
